29bf2d1cb3e1c222a5a923989be3b7c86b6cef64
ayenuaa <825032664@qq.com>
2017-04-02 19:58:10 +0800

    Update tradespi.h

--- tradespi.h
+++ tradespi.h
@@ -5,93 +5,88 @@
 #include <ThostFtdcUserApiDataType.h>
 #include <ThostFtdcUserApiStruct.h>
 
-class TradeSpi:public CThostFtdcTraderSpi
+class TradeSpi : public CThostFtdcTraderSpi
 {
 public:
-    //构造函数
-        TradeSpi(CThostFtdcTraderApi *tdapi);
+    ///当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
+    virtual void OnFrontConnected();
 
-        //当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
-        void OnFrontConnected();
+    ///登录请求响应
+    virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,	CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
-        void OnFrontDisconnected(int nReason) override;
-        //登录请求响应
-        void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,
-                CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    ///投资者结算结果确认响应
+    virtual void OnRspSettlementInfoConfirm(CThostFtdcSettlementInfoConfirmField *pSettlementInfoConfirm, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
+    ///请求查询合约响应
+    virtual void OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
+    ///请求查询资金账户响应
+    virtual void OnRspQryTradingAccount(CThostFtdcTradingAccountField *pTradingAccount, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
-        //请求查询合约
-       void OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo,
-                                        int nRequestID, bool bIsLast) override;
+    ///请求查询投资者持仓响应
+    virtual void OnRspQryInvestorPosition(CThostFtdcInvestorPositionField *pInvestorPosition, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
+    ///报单录入请求响应
+    virtual void OnRspOrderInsert(CThostFtdcInputOrderField *pInputOrder, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
-        //投资者结算结果确认响应
-        void OnRspSettlementInfoConfirm(CThostFtdcSettlementInfoConfirmField *pSettlementInfoConfirm,
-            CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+    ///报单操作请求响应
+    virtual void OnRspOrderAction(CThostFtdcInputOrderActionField *pInputOrderAction, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
+    ///错误应答
+    virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
 
-        //请求查询行情响应
-        void OnRspQryDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData,
-            CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    ///当客户端与交易后台通信连接断开时，该方法被调用。当发生这个情况后，API会自动重新连接，客户端可不做处理。
+    virtual void OnFrontDisconnected(int nReason);
 
-        //请求查询投资者持仓响应
-        void OnRspQryInvestorPosition(CThostFtdcInvestorPositionField *pInvestorPosition,
-            CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+    ///心跳超时警告。当长时间未收到报文时，该方法被调用。
+    virtual void OnHeartBeatWarning(int nTimeLapse);
 
+    ///报单通知
+    virtual void OnRtnOrder(CThostFtdcOrderField *pOrder);
 
+    ///成交通知
+    virtual void OnRtnTrade(CThostFtdcTradeField *pTrade);
+    ///请求查询报单响应
+    virtual void OnRspQryOrder(CThostFtdcOrderField *pOrder, CThostFtdcRspInfoField *pRspInfo,
+                       int nRequestID, bool bIsLast) override;
 
-        //查询资金帐户响应
-        void OnRspQryTradingAccount(CThostFtdcTradingAccountField *pTradingAccount,
-            CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
-
-        //报单录入错误的时候相应
-        void OnRspOrderInsert(CThostFtdcInputOrderField *pInputOrder, CThostFtdcRspInfoField *pRspInfo,
-                                          int nRequestID, bool bIsLast) override;
-        //报单回报
-        void OnRtnOrder(CThostFtdcOrderField *pOrder) override;
-
-        //成交通知
-       void OnRtnTrade(CThostFtdcTradeField *pTrade) override;
-
-       //请求查询报单响应
-       void OnRspQryOrder(CThostFtdcOrderField *pOrder, CThostFtdcRspInfoField *pRspInfo,
-                          int nRequestID, bool bIsLast) override;
-
-       //请求查询成交响应
-       void OnRspQryTrade(CThostFtdcTradeField *pTrade, CThostFtdcRspInfoField *pRspInfo,
-                          int nRequestID, bool bIsLast) override;
+    ///请求查询成交响应
+    virtual void OnRspQryTrade(CThostFtdcTradeField *pTrade, CThostFtdcRspInfoField *pRspInfo,
+                       int nRequestID, bool bIsLast) override;
 
 private:
-        //用户登录请求
-        void ReqUserLogin();
-        //用户查询行情
-        void RspQryDepthMarketData();
-        //投资者结算结果确认
-        void ReqSettlementInfoConfirm();
-        //请求查询合约
-        void ReqQryInstrument();
-        //请求查询资金账户
-        void ReqQryTradingAccount();
-        //请求查询投资者持仓
-        void ReqQryInvestorPosition();
-        //报单录入请求
-        void ReqOrderInsert();
-        //报单操作请求
-        void ReqOrderAction(CThostFtdcOrderField *pOrder);
-        //请求查询报单
-        void RspQryOrder();
-        //请求查询成交
-        void RspQryTrade();
-        // 是否收到成功的响应
-        bool IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo);
-        // 是否我的报单回报
-        bool IsMyOrder(CThostFtdcOrderField *pOrder);
-        // 是否正在交易的报单
-        bool IsTradingOrder(CThostFtdcOrderField *pOrder);
-        CThostFtdcTraderApi *tdapi;
-        CThostFtdcReqUserLoginField *loginField;
-        CThostFtdcReqAuthenticateField *authField;
+    ///用户登录请求
+    void ReqUserLogin();
+    ///投资者结算结果确认
+    void ReqSettlementInfoConfirm();
+    ///请求查询合约
+    void ReqQryInstrument();
+    ///请求查询资金账户
+    void ReqQryTradingAccount();
+    ///请求查询投资者持仓
+    void ReqQryInvestorPosition();
+    ///报单录入请求
+    void ReqOrderInsert();
+    ///报单操作请求
+    void ReqOrderAction(CThostFtdcOrderField *pOrder);
+    ///请求查询报单
+    void RspQryOrder();
+    ///请求查询成交
+    void RspQryTrade();
+    //void ReqOrderAction();
+
+    ///报单录入请求，开仓
+    void ReqOrderOpen();
+
+    ///报单录入请求，平仓
+    void ReqOrderClose(double price, int number, char direction);
+
+    // 是否收到成功的响应
+    bool IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo);
+    // 是否我的报单回报
+    bool IsMyOrder(CThostFtdcOrderField *pOrder);
+    // 是否正在交易的报单
+    bool IsTradingOrder(CThostFtdcOrderField *pOrder);
 };
 
 #endif // TRADESPI_H
